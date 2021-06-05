2c0b444f57d05885ee8d42d219e395d8bb66babf
Jake Grella <jgrella13@gmail.com>
2021-06-05 11:56:52 -0500

    add trackable currencies

---
+++ src/currencies.js
@@ -0,0 +1,49 @@
+export const currencies = [
+  {
+    name: 'all',
+    symbol: 'all',
+    iconUrl: 'https://i.imgur.com/d1yRWXu.png'
+  },
+  {
+    name: 'bitcoin',
+    symbol: 'btc',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png'
+  },
+  {
+    name: 'ethereum',
+    symbol: 'eth',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png'
+  },
+  {
+    name: 'tether',
+    symbol: 'usdt',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/41f6a93a3a222078c939115fc304a67c384886b7a9e6c15dcbfa6519dc45f6bb4a586e9c48535d099efa596dbf8a9dd72b05815bcd32ac650c50abb5391a5bd0/asset_icons/1f8489bb280fb0a0fd643c1161312ba49655040e9aaaced5f9ad3eeaf868eadc.png'
+  },
+  {
+    name: 'binance usd',
+    symbol: 'busd',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/2abc930c5ffe8a31546ae668f3f34830cea5311ad3b08ca747c15f50eb110c2805073397c8d7892bdc5b0937f4c7cf2fedb785653a342e979ab916e58b404bb4/asset_icons/557ba984249638fc434becf23356c67d26b5ca6bb08d095c2a2a80ae7b0cbde8.png'
+  },
+  {
+    name: 'binance coin',
+    symbol: 'bnb',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/4861e50787caa9405703c71e788467e8242f5d15a7a51335c299dc3e87a8d1d08bfd19ab67ad8bb2581b525af27c8dcbd0c78ede837eeaff75ae9b96716bf75e/asset_icons/1597d628dd19b7885433a2ac2d7de6ad196c519aeab4bfe679706aacbf1df78a.png'
+  },
+  {
+    name: 'chainlink',
+    symbol: 'link',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/9e34c547e363744ad9e5d140d098f498ee8959a2c175366765beb000c24695712697b3aa8d972e75e4d6c79a5c16e496a78fe30094638b1ad5fea45db71c82df/asset_icons/351b8b5f92576806a20692c98ae00c4d930117e44b26f217ab2bef5b29b3e312.png'
+  },
+  {
+    name: 'xrp',
+    symbol: 'xrp',
+    iconUrl:
+      'https://dynamic-assets.coinbase.com/e81509d2307f706f3a6f8999968874b50b628634abf5154fc91a7e5f7685d496a33acb4cde02265ed6f54b0a08fa54912208516e956bc5f0ffd1c9c2634099ae/asset_icons/3af4b33bde3012fd29dd1366b0ad737660f24acc91750ee30a034a0679256d0b.png'
+  }
+];
