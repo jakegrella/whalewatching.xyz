ad905bf640067b75311794c399c1336b92374100
Jake Grella <jgrella13@gmail.com>
2021-06-06 17:16:03 -0500

    add state vals

--- src/context/WhaleProvider.js
+++ src/context/WhaleProvider.js
@@ -4,7 +4,9 @@ import WhaleContext from './WhaleContext';
 const WhaleProvider = (props) => {
   const initialState = {
     currency: 'all',
-    minTransactionVal: '500000'
+    minTransactionVal: '500000',
+    transactionCount: 0,
+    transactions: []
   };
 
   const [Whales, setWhales] = useState(initialState);
