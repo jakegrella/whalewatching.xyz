88b244761d3d6f259d48dd145736fc52a5ea62f1
Jake Grella <jgrella13@gmail.com>
2021-06-06 17:49:53 -0500

    remove style imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Header from './components/Header';
 import Options from './components/Options';
 import WhaleContainer from './components/WhaleContainer';
 import { EuiFlexGroup } from '@elastic/eui';
-import './reset.scss';
-import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
-import './styles.scss';
 
 function App() {
   return (
