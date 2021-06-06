c7ab327f66048bba16476f29b4588b93e8f7f6af
Jake Grella <jgrella13@gmail.com>
2021-06-06 17:17:45 -0500

    add eui comp for flex

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,15 @@
+import { EuiFlexGroup } from '@elastic/eui';
 import CurrencySelect from './CurrencySelect';
 import MinValSlider from './MinValSlider';
 import SubmitBtn from './submitBtn';
 
 const Options = () => {
   return (
-    <>
+    <EuiFlexGroup alignItems='center'>
       <CurrencySelect />
       <MinValSlider />
       <SubmitBtn />
-    </>
+    </EuiFlexGroup>
   );
 };
 
