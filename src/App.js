8a368d3c10465432120c6c0d81434d381df33b6f
Jake Grella <jgrella13@gmail.com>
2021-06-06 17:18:29 -0500

    implement new containers and begin styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
-
 import Header from './components/Header';
 import Options from './components/Options';
+import WhaleContainer from './components/WhaleContainer';
+import { EuiFlexGroup } from '@elastic/eui';
+import './reset.scss';
+import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
+import './styles.scss';
 
 function App() {
   return (
-    <>
+    <EuiFlexGroup alignItems='center' direction='column' className='app'>
       <Header />
       <Options />
-    </>
+      <WhaleContainer />
+    </EuiFlexGroup>
   );
 }
 
