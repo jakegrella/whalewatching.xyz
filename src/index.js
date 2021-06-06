414d19cbc7dffc63c79b2189471934e6be4bab19
Jake Grella <jgrella13@gmail.com>
2021-06-06 18:05:00 -0500

    update styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import WhaleProvider from './context/WhaleProvider';
 import './style/main.scss';
-import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
+// import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
 
 ReactDOM.render(
   <React.StrictMode>
