59e2a009940dc82ed648689e296f9b0b70915de3
Jake Grella <jgrella13@gmail.com>
2021-06-06 18:09:53 -0500

    remove unnecessary styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import WhaleProvider from './context/WhaleProvider';
-import './style/main.scss';
-// import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
+import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
 
 ReactDOM.render(
   <React.StrictMode>
