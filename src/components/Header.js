b3ddfc57142c525c156e32d37c6da9098a07b266
Jake Grella <jgrella13@gmail.com>
2021-06-06 19:32:48 -0500

    add copy

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,23 @@ const Header = () => {
     <EuiFlexItem>
       <header>
         <EuiText>
-          <h1>whalewatching.xyz</h1>
+          <h1>🐋 whalewatching.xyz</h1>
+          <h3>view the largest blockchain transactions from the past hour</h3>
+          <p>
+            In the crypto space, an account that holds a large amount of bitcoin
+            or other cryptocurrency is known as a whale. This app allows a user
+            to view blockchain whale account transactions that exceed $500,000.
+            This is possible because the transaction details of the listed
+            cryptocurrencies are publicly available.
+          </p>
+          <p>
+            Data is being referenced through the{' '}
+            <a href='http://whale-alert.io' target='_blank' rel='noreferrer'>
+              whale-alert.io
+            </a>{' '}
+            API.
+          </p>
+          <p>I call it "whale watching".</p>
         </EuiText>
       </header>
     </EuiFlexItem>
