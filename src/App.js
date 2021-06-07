6e0d6b495026033f295ebdd7995c558760fdcf7b
Jake Grella <jgrella13@gmail.com>
2021-06-06 19:32:42 -0500

    import style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Header from './components/Header';
 import Options from './components/Options';
 import WhaleContainer from './components/WhaleContainer';
 import { EuiFlexGroup } from '@elastic/eui';
+import './style.scss';
 
 function App() {
   return (
-    <EuiFlexGroup alignItems='center' direction='column' className='app'>
+    <EuiFlexGroup direction='column' className='app'>
       <Header />
       <Options />
       <WhaleContainer />
