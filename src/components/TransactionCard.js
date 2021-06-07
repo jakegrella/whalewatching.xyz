0ec96b22b1112e8fb73d9f07db4eedb4c2ccfac5
Jake Grella <jgrella13@gmail.com>
2021-06-06 21:03:25 -0500

    add EuiFlex components to help with display

--- src/components/TransactionCard.js
+++ src/components/TransactionCard.js
@@ -1,37 +1,58 @@
-import { EuiCard, EuiButton } from '@elastic/eui';
+import { EuiCard, EuiFlexGroup, EuiFlexItem, EuiButton } from '@elastic/eui';
 
 const TransactionCard = (props) => {
   const { transaction } = props;
+
   return (
-    <EuiCard title={`${transaction.hash.substr(0, 20)}...`}>
-      <p>
-        {transaction.amount} {transaction.symbol}
-      </p>
-      <p>${transaction.amount_usd} USD</p>
-      <p>
-        From:{' '}
-        <a
-          href={`https://www.blockchain.com/${transaction.symbol}/address/${transaction.from.address}`}
-          target='_blank'
-          rel='noreferrer'>
-          {transaction.from.address.substr(0, 20)}...
-        </a>{' '}
-        ({transaction.from.owner_type} wallet)
-      </p>
-      <p>
-        To:{' '}
-        <a
-          href={`https://www.blockchain.com/${transaction.symbol}/address/${transaction.to.address}`}
-          target='_blank'
-          rel='noreferrer'>
-          {transaction.to.address.substr(0, 20)}...
-        </a>{' '}
-        ({transaction.to.owner_type} wallet)
-      </p>
-      <EuiButton
-        href={`http://blockchain.com/${transaction.symbol}/tx/${transaction.hash}`}>
-        more info
-      </EuiButton>
+    <EuiCard title={`${transaction.hash.substr(0, 20)}...`} textAlign='left'>
+      <EuiFlexGroup alignItems='flexStart' justifyContent='flexStart'>
+        <EuiFlexItem grow={2}>
+          <p>
+            Amount ({transaction.symbol.toUpperCase()}): {transaction.amount}{' '}
+            {transaction.symbol.toUpperCase()}
+          </p>
+          <p>
+            Amount (USD): $
+            {new Intl.NumberFormat('en-US').format(transaction.amount_usd)}
+          </p>
+        </EuiFlexItem>
+        <EuiFlexItem grow={4}>
+          <p>
+            From:{' '}
+            <a
+              href={`https://www.blockchain.com/${transaction.symbol}/address/${transaction.from.address}`}
+              target='_blank'
+              rel='noreferrer'>
+              {transaction.from.address.substr(0, 20)}...
+            </a>{' '}
+            (
+            {transaction.from.owner
+              ? transaction.from.owner
+              : transaction.from.owner_type}{' '}
+            wallet)
+          </p>
+          <p>
+            To:{' '}
+            <a
+              href={`https://www.blockchain.com/${transaction.symbol}/address/${transaction.to.address}`}
+              target='_blank'
+              rel='noreferrer'>
+              {transaction.to.address.substr(0, 20)}...
+            </a>{' '}
+            (
+            {transaction.to.owner
+              ? transaction.to.owner
+              : transaction.to.owner_type}{' '}
+            wallet)
+          </p>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiButton
+            href={`http://blockchain.com/${transaction.symbol}/tx/${transaction.hash}`}>
+            more info
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </EuiCard>
   );
 };
